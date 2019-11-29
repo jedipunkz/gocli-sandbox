e0a81382f1a708eefe185d7347109143791b8747
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2019-11-29 23:12:40 +0900

    add root.go

---
+++ cmd/root.go
@@ -0,0 +1,15 @@
+var rootCmd = &cobra.Command{
+  Use:   "GoCLI-sandbox",
+  Short: "GO CLI Study",
+  Long: `GoCLI : CLI Sandbox for studying golang`,
+  Run: func(cmd *cobra.Command, args []string) {
+    // Do Stuff Here
+  },
+}
+
+func Execute() {
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+}
