59657ff9b495273957d307c1756aaf22182ea015
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2019-11-29 23:51:45 +0900

    add viper

--- cmd/version.go
+++ cmd/version.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/fatih/color"
 )
 
 func init() {
+	viper.SetDefault("TestVar", "foo")
 	rootCmd.AddCommand(versionCmd)
 }
 
@@ -17,6 +19,16 @@ var versionCmd = &cobra.Command{
 	Long:	`All Software has versions, This is GoCLI-sandbox`,
 	Run:	func(cmd *cobra.Command, args []string) {
 		cyan := color.New(color.FgCyan).SprintFunc()
-		fmt.Println(cyan("GoCLI-sandbox CLI Study Sample v0.1 -- HEAD"))
+		v := viper.New()
+		v.SetConfigName(".gocli-sandbox")
+		v.AddConfigPath("$HOME")
+		err = v.ReadInConfig()
+		if err != nil {
+			panic(fmt.Errorf("Fatal errror config file %s \n", err))
+		}
+
+		author = v.GetString("author")
+
+		fmt.Println(cyan("GoCLI-sandbox CLI Study Sample v0.1 -- HEAD" + " " + author))
 	},
 }
